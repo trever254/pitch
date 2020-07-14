0549c454cc355a72e3038bc5d5b1381a8f476a86
trever <toakumu@gmail.com>
2020-07-14 14:49:39 +0300

    added form.py

---
+++ app/main/forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField, ValidationError, BooleanField, TextAreaField, SelectField, RadioField
+from wtforms.validators import Required
+
+class
