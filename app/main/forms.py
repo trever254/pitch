c4ffaa223e1139d19a42d2d344504887f843c0ba
trever <toakumu@gmail.com>
2020-07-14 14:58:02 +0300

    added views.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,4 +2,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField, ValidationError, BooleanField, TextAreaField, SelectField, RadioField
 from wtforms.validators import Required
 
-class
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators=[Required()])
+    submit = SubmitField('Submit')
+
+#Pitch Form
+class PitchForm(FlaskForm):
+    content = TextAreaField('Post a pitch')
+    submit = SubmitField('Submit')
+
+#Comment Form
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField()
+    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
+
+#categoryForm
+class CategoryForm(FlaskForm):
+    name = TextAreaField('Category')
+    submit = SubmitField()
+
+         
