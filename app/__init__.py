f2a5a8a445dee686cb2ba0392c324aa1049af8f0
trever <toakumu@gmail.com>
2020-07-14 08:53:39 +0300

    added create app function

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,37 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 # from flask_simplemde import SimpleMDE
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos', IMAGES)
+# simple = SimpleMDE
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    # simple.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    return app
