5db565fc1bdaaa47c87937ee9057f03e9d96710c
trever <toakumu@gmail.com>
2020-07-17 08:54:36 +0300

    added class config in config.py file

--- config.py
+++ config.py
@@ -2,4 +2,34 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://trever:password@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+# email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
+    DEBUG = True
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://trever:password@localhost/pitch'
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
+
+
+
+
