5cacbc464a41e5735faf53f4d35ca58cc15c2bbe
trever <toakumu@gmail.com>
2020-07-23 12:33:29 +0300

    added changes

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager,Server
 from app.models import User, Pitch, Category, Vote, Comment
 
 # creating an app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
